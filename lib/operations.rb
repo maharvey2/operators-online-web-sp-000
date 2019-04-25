c8df53c8a1faa87f2b49c088edd314fa3f8b106b
Mark Harvey <maharvey2@crimson.ua.edu>
2019-04-25 17:48:05 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,18 @@
+
+
+#Where you use if, else, elsif
 def unsafe?(speed)
-
+	if speed > 60
+		true
+	elsif speed < 40
+		true
+	else
+		false
+	end
 end
 
 
-
+#where you use the ?: ternary operator
 def not_safe?(speed)
-	
+	speed < 40 || speed > 60 ? true : false
 end
-	
-
-
